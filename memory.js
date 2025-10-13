afd9265cc43bfe8efe94d1a4a0d65b1d070cfb6e
fanisridhar <99811717+fanisridhar@users.noreply.github.com>
2025-10-13 13:52:25 -0400

    Create memory.js

---
+++ memory.js
@@ -0,0 +1,16 @@
+export class Memory {
+  constructor(state) { this.state = state; }
+  async fetch(request) {
+    const url = new URL(request.url);
+    if (url.pathname === "/get") {
+      const history = (await this.state.storage.get("history")) || [];
+      return new Response(JSON.stringify(history), { headers: { "content-type": "application/json" }});
+    }
+    if (url.pathname === "/save") {
+      const data = await request.json();
+      await this.state.storage.put("history", data);
+      return new Response("ok");
+    }
+    return new Response("Not found", { status: 404 });
+  }
+}
